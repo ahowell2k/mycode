afefa44da576a0de938d49d2e75f37924cf53851
Aaron Howell <ahowell2k@alta3.com>
2024-11-07 18:20:38 +0000

    Day 4 Morning Commit

---
+++ challenges/superhero.py
@@ -0,0 +1,141 @@
+
+
+classinfo = {
+    "all": [
+        {
+            "name": "Aaron",
+            "skill level": "wondrous",
+            "spirit animal": "Alpaca",
+            "super power": "Structure Weakening",
+        },
+        {
+            "name": "Ahamed",
+            "skill level": "admirable",
+            "spirit animal": "Shark",
+            "super power": "Super Strength",
+        },
+        {
+            "name": "Alice",
+            "skill level": "amazing",
+            "spirit animal": "Goat",
+            "super power": "Weather Control",
+        },
+        {
+            "name": "Angela",
+            "skill level": "astonishing",
+            "spirit animal": "Banana",
+            "super power": "Flight",
+        },
+        {
+            "name": "Ayrat",
+            "skill level": "awesome",
+            "spirit animal": "Horse",
+            "super power": "X-ray Vision",
+        },
+        {
+            "name": "Brandon",
+            "skill level": "brilliant",
+            "spirit animal": "Eagle",
+            "super power": "Shape of: A Giant Shark",
+        },
+        {
+            "name": "Brian",
+            "skill level": "cool",
+            "spirit animal": "Rabbit",
+            "super power": "Invisibility",
+        },
+        {
+            "name": "Carl",
+            "skill level": "enjoyable",
+            "spirit animal": "Water buffalo",
+            "super power": "Transformation",
+        },
+        {
+            "name": "Chris",
+            "skill level": "excellent",
+            "spirit animal": "Chicken",
+            "super power": "Pyrokinesis",
+        },
+        {
+            "name": "Cliff",
+            "skill level": "fabulous",
+            "spirit animal": "Duck",
+            "super power": "Invulnerability",
+        },
+        {
+            "name": "David",
+            "skill level": "fantastic",
+            "spirit animal": "Goose",
+            "super power": "Explosive Shouts",
+        },
+        {
+            "name": "Erin",
+            "skill level": "great",
+            "spirit animal": "Pigeon",
+            "super power": "Matter Ingestion",
+        },
+        {
+            "name": "Felicia",
+            "skill level": "incredible",
+            "spirit animal": "Turkey",
+            "super power": "Zoolingualism",
+        },
+        {
+            "name": "Gabriel",
+            "skill level": "magnificent",
+            "spirit animal": "Aardvark",
+            "super power": "Height Manipulation",
+        },
+        {
+            "name": "Jeff",
+            "skill level": "marvelous",
+            "spirit animal": "Aardwolf",
+            "super power": "Hydrokinesis",
+        },
+        {
+            "name": "Justin",
+            "skill level": "outstanding",
+            "spirit animal": "Elephant",
+            "super power": "Needle Projection",
+        },
+        {
+            "name": "Linda",
+            "skill level": "phenomenal",
+            "spirit animal": "Leopard",
+            "super power": "Stretchy",
+        },
+        {
+            "name": "Michael",
+            "skill level": "pleasant",
+            "spirit animal": "Albatross",
+            "super power": "Steel Skin",
+        },
+        {
+            "name": "Namit",
+            "skill level": "pleasing",
+            "spirit animal": "Alligator",
+            "super power": "Teleportation",
+        },
+        {
+            "name": "Shenouda",
+            "skill level": "remarkable",
+            "spirit animal": "Lynx",
+            "super power": "Polyglot",
+        },
+        {
+            "name": "Zachary",
+            "skill level": "super",
+            "spirit animal": "Fox",
+            "super power": "Eat Anything",
+        },
+    ]
+}
+
+
+
+name = classinfo['all'][0]['name']
+power = classinfo['all'][0]['super power']
+
+print(name, "has the power of", power)
+
+for student in classinfo:
